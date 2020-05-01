b4a014f2137c9da45aa69c6f9ca68a961e86366d
ravi chandola <ravichandola94@gmail.com>
2020-05-02 03:59:53 +0530

    Fixed the decreaseQuantity functionality

--- src/CartItem.js
+++ src/CartItem.js
@@ -24,6 +24,10 @@ class CartItem extends React.Component {
 
 
 decreaseQuantity = () =>{
+  const {qty} = this.state;
+  if(qty===0){
+    return;
+  }
   this.setState((prevState) => {
     return{
       qty : prevState.qty-1
@@ -35,7 +39,8 @@ decreaseQuantity = () =>{
 
 
   render () {
-    const { price, title, qty } = this.state;
+
+      const { price, title, qty } = this.state;
     return (
       <div className="cart-item">
         <div className="left-block">
@@ -49,7 +54,7 @@ decreaseQuantity = () =>{
             {/* Buttons */}
             <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/svg/992/992651.svg" onClick={this.increaseQuantity}/>
             <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/svg/1665/1665612.svg" onClick={this.decreaseQuantity} />
-            <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/svg/1214/1214428.svg" />
+            <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/svg/1214/1214428.svg" onClick={this.handleClick}/>
           </div>
         </div>
       </div>
