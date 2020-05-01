ebf03650f5906de10d4627078cf8c14688da4908
ravi chandola <ravichandola94@gmail.com>
2020-05-02 02:25:41 +0530

    Added state to the cart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import  './App.css';
 import CartItem from './CartItem';
 
 function App() {
   return (
     <div className="App">
-     <CartItem/>
-      </div>
+      <CartItem />
+    </div>
   );
 }
 
